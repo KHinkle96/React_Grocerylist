9f96156938b8fb0c760c2847c3ee49464ea7c325
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-14 20:43:11 -0800

    Added the rest of the button event so that when the button is clicked the grocerys are displayed below.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 
 const App = ()=> {
-  const[textareaValue, setTextareaValue] = useState("")
-  const[grocerys, setGrocerys] = useState([])
+  const [textareaValue, setTextareaValue] = useState("")
+  const [grocerys, setGrocerys] = useState([])
   const listgrocerys = grocerys.map((grocery, i)=> <li key={i}>{grocery}</li>)
 
   const storeEnteredText = (event)=> {
     setTextareaValue(event.target.value)
   }
   const handleButtonClick = (event)=> {
-    
+    setGrocerys([...grocerys, textareaValue])
   }
 
   return (
