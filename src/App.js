1161370890b5909ac510ce785eafb8a18469973e
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-16 21:14:31 -0800

    added some more css for the text box and button, added top text asking the user to enter an item and price. Added text box placeholder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 const App = ()=> {
   const [textareaValue, setTextareaValue] = useState("")
   const [grocerys, setGrocerys] = useState([])
-  const listgrocerys = grocerys.map((grocery, i)=> <li key={i}>{grocery} <input type="checkbox"/> </li>)
+  const listgrocerys = grocerys.map((grocery, i)=> <li key={i}>{grocery}<input type="checkbox"/> </li>)
 
   const storeEnteredText = (event)=> {
     setTextareaValue(event.target.value)
@@ -15,9 +15,12 @@ const App = ()=> {
   return (
   <div className="App">
     <h1 className="title">Grocery List App</h1>
-    <textarea onChange={storeEnteredText}></textarea>
     <div>
-    <button onClick={handleButtonClick}>Add Item</button>
+    <p>Enter an Item and Price:</p>
+    <textarea placeholder="Bananas $1, Steak $9, Yogurt $1.50..." className="groceryitemsbox"onChange={storeEnteredText}></textarea>
+    </div>
+    <div>
+    <button class="button" onClick={handleButtonClick}>Add Item</button>
     </div>
     <ol>
       {listgrocerys}
